47405f7fc3e2f8905ba6cb6496512b3ee9deae94
enchenu <enchenu@gmail.com>
2015-11-22 10:54:57 -0300

    encheu@gmail.com

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :cities
+  resources :states
+  resources :countries
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
